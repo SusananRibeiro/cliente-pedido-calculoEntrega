5ad67128f88694cb597563bff7b28c51ee3986b9
Susana_Ribeiro <susanadonribeiro@gmail.com>
2023-11-02 21:16:17 -0300

    Criacao da classe ProdutosController

--- src/main/java/com/cadastro/useCases/produtos/domanis/ProdutosResponseDom.java
+++ src/main/java/com/cadastro/useCases/produtos/domanis/ProdutosResponseDom.java
@@ -1,9 +1,17 @@
 package com.cadastro.useCases.produtos.domanis;
 
 public class ProdutosResponseDom {
+    private Long id;
     private String nome;
-
     private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getNome() {
         return nome;
